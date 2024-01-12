2272ce33ceaeeaa483e3fb48698e38befa7892b1
황동규 <121680372+Hwangdonggyu@users.noreply.github.com>
2024-01-12 13:43:11 +0900

    Title: 기수 정렬 구현, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240112/기수 정렬 구현/implement-radix-sort.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+def radixSort():
+    n = int(input())
+    nums = list(map(int, input().split(' ')))
+    buckets = [deque() for _ in range(10)]
+    
+    max_val = max(nums)
+    queue = deque(nums)
+    digit = 1 # 자릿수
+    
+    while (max_val >= digit): # 가장 큰 수의 자릿수일 때 까지만 실행
+        while queue:
+            num = queue.popleft()
+            buckets[(num // digit) % 10].append(num) # 각 자리의 수(0~9)에 따라 버킷에 num을 넣는다.
+        
+        # 해당 자릿수에서 버킷에 다 넣었으면, 버킷에 담겨있는 순서대로 꺼내와 정렬한다.
+        for bucket in buckets:
+            while bucket:
+                queue.append(bucket.popleft())
+
+        digit *= 10 # 자릿수 증가시키기
+    
+    queue = list(queue)
+    for i in range(n):
+        print(queue[i], end = " ")
+radixSort()
