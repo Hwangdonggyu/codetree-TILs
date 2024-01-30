a8f6b1894945493926f6e7d9722b1f5ea1e873e0
황동규 <121680372+Hwangdonggyu@users.noreply.github.com>
2024-01-30 13:41:12 +0900

    Title: 퀵 정렬 구현, Time: 414ms, Memory: 81MB, Status: Passed - CodeTree

---
+++ 240130/퀵 정렬 구현/implement-quick-sort.py
@@ -0,0 +1,19 @@
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    lesser_arr, equal_arr, greater_arr = [], [], []
+    for num in arr:
+        if num < pivot:
+            lesser_arr.append(num)
+        elif num > pivot:
+            greater_arr.append(num)
+        else:
+            equal_arr.append(num)
+    return quick_sort(lesser_arr) + equal_arr + quick_sort(greater_arr)
+
+n = int(input())
+arr = list(map(int,input().split()))
+arr = quick_sort(arr)
+for i in range(n):
+    print(arr[i], end = " ")
